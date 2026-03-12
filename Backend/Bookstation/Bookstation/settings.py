3ef89b8856b59b49ec8a1885c64379d46a3db2fc
swathikrishna111530-png <swathikrishna111530@gmail.com>
2026-03-12 23:09:13 +0530

    enabled cors for vercel frontend

--- Backend/Bookstation/Bookstation/settings.py
+++ Backend/Bookstation/Bookstation/settings.py
@@ -59,6 +59,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://fullstackproject1-jiiq.vercel.app",
+]
+
 # CSRF_TRUSTED_ORIGINS = [
 #     "http://localhost:5173",
 #     "http://127.0.0.1:5173",
